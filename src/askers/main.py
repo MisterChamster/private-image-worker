7db7cd4e1e191d83bb33aa2c26d19822ebac559d
Mr Chamster <s.duda03@gmail.com>
2025-08-03 22:39:13 +0200

    Add ask_path_filedialog asker

--- src/askers/main.py
+++ src/askers/main.py
@@ -1,4 +1,5 @@
 import os
+from tkinter import filedialog
 
 
 
@@ -16,6 +17,21 @@ def ask_mainloop_action():
             print("Incorrect input.\n")
         else:
             return action
+
+
+def ask_path_filedialog(type, message):
+    original_path = os.getcwd()
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    os.chdir(desktop_path)
+
+    sel_path = ""
+    if type == "f":
+        sel_path = filedialog.askopenfilename(title=message)
+    elif type == "d":
+        sel_path = filedialog.askdirectory(title=message)
+
+    os.chdir(original_path)
+    return sel_path
 
 
 def ask_path():
