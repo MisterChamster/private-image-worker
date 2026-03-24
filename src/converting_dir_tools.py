ce9330a99562802a1404d7f2d8c4fc43b1726f96
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-24 22:28:39 +0100

    Merge file conv functions

--- src/converting_dir_tools.py
+++ src/converting_dir_tools.py
@@ -5,7 +5,7 @@ import src.converting_file_tools as conv_file
 
 
 
-def HEICtoPNG_dir_no_del(images_dir: Path) -> None:
+def HEICtoPNG_dir(images_dir: Path, del_flag: bool = False) -> None:
     for file_path in images_dir.iterdir():
         ext = file_path.suffix
         if ext.lower() != ".heic":
@@ -13,22 +13,14 @@ def HEICtoPNG_dir_no_del(images_dir: Path) -> None:
 
         conv_file.HEICtoPNG(file_path)
 
-
-def HEICtoPNG_dir_del(images_dir: Path) -> None:
-    for file_path in images_dir.iterdir():
-        ext = file_path.suffix
-        if ext.lower() != ".heic":
-            continue
-
-        conv_file.HEICtoPNG(file_path)
-
-        try:
-            os.remove(file_path)
-        except:
-            print("Couldn't remove " + file_path.name)
+        if del_flag:
+            try:
+                os.remove(file_path)
+            except:
+                print("Couldn't remove " + file_path.name)
 
 
-def HEICtoJPG_dir_no_del(images_dir: Path) -> None:
+def HEICtoJPG_dir(images_dir: Path, del_flag: bool = False) -> None:
     for file_path in images_dir.iterdir():
         ext = file_path.suffix
         if ext.lower() != ".heic":
@@ -36,22 +28,14 @@ def HEICtoJPG_dir_no_del(images_dir: Path) -> None:
 
         conv_file.HEICtoJPG(file_path)
 
-
-def HEICtoJPG_dir_del(images_dir: Path) -> None:
-    for file_path in images_dir.iterdir():
-        ext = file_path.suffix
-        if ext.lower() != ".heic":
-            continue
-
-        conv_file.HEICtoJPG(file_path)
-
-        try:
-            os.remove(file_path)
-        except:
-            print("Couldn't remove " + file_path.name)
+        if del_flag:
+            try:
+                os.remove(file_path)
+            except:
+                print("Couldn't remove " + file_path.name)
 
 
-def PNGtoJPG_dir_no_del(images_dir: Path) -> None:
+def PNGtoJPG_dir(images_dir: Path, del_flag: bool = False) -> None:
     for file_path in images_dir.iterdir():
         ext = file_path.suffix
         if ext.lower() != ".png":
@@ -59,16 +43,8 @@ def PNGtoJPG_dir_no_del(images_dir: Path) -> None:
 
         conv_file(file_path)
 
-
-def PNGtoJPG_dir_del(images_dir: Path) -> None:
-    for file_path in images_dir.iterdir():
-        ext = file_path.suffix
-        if ext.lower() != ".png":
-            continue
-
-        conv_file(file_path)
-
-        try:
-            os.remove(file_path)
-        except:
-            print("Couldn't remove " + file_path.name)
+        if del_flag:
+            try:
+                os.remove(file_path)
+            except:
+                print("Couldn't remove " + file_path.name)
