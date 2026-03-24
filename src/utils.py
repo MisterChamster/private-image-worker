535512672598edfc9dc3dc20144302a061c64136
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-24 14:16:22 +0100

    Last fixes

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+from typing import Literal
 
 
 
-def list_images_in_dir(dir_path: Path, format: str = "all") -> None:
+def list_images_in_dir(
+        dir_path: Path,
+        format: Literal["all", "heic", "png", "jpg"] = "all"
+        ) -> None:
     """Lists all images in current directory with specified format."""
     valid_extensions = ()
     if format == "all":
@@ -11,9 +15,12 @@ def list_images_in_dir(dir_path: Path, format: str = "all") -> None:
         valid_extensions = ('.heic')
     elif format == "png":
         valid_extensions = ('.png')
+    elif format == "jpg":
+        valid_extensions = ('.jpg', '.jpeg')
     else:
         raise Exception("module_main.py/list_images_in_dir error: Wrong format chosen.")
 
     for file_path in dir_path.iterdir():
-        if file_path.suffix.lower() in valid_extensions:
+        if (file_path.suffix.lower() in valid_extensions and
+            not file_path.stem.startswith(".")):
             print(file_path)
