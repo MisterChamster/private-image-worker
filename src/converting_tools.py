933fbf265255f53bb5bc8f5bc679984e2de9e106
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-24 21:42:28 +0100

    Get out of cond block

--- src/converting_tools.py
+++ src/converting_tools.py
@@ -10,20 +10,21 @@ def HEICtoPNG_no_del(images_dir: Path) -> None:
         file_name = file_path.name
         ext = file_path.suffix
 
-        # TEMPPPPPP reverse condition
-        if ext.lower() == ".heic":
-            # create an Image object from the HEIC file
-            print("Converting:", file_name)
-            heif_file = pillow_heif.read_heif(file_path)
-            image = Image.frombytes(
-                heif_file.mode,
-                heif_file.size,
-                heif_file.data,
-                "raw",)
+        if ext.lower() != ".heic":
+            continue
 
-            new_filename = file_path.stem + ".png"
-            new_filepath = images_dir / new_filename
-            image.save(new_filepath, format("png"))
+        # create an Image object from the HEIC file
+        print("Converting:", file_name)
+        heif_file = pillow_heif.read_heif(file_path)
+        image = Image.frombytes(
+            heif_file.mode,
+            heif_file.size,
+            heif_file.data,
+            "raw",)
+
+        new_filename = file_path.stem + ".png"
+        new_filepath = images_dir / new_filename
+        image.save(new_filepath, format("png"))
 
 
 def HEICtoPNG_del(images_dir: Path) -> None:
@@ -31,29 +32,30 @@ def HEICtoPNG_del(images_dir: Path) -> None:
         file_name = file_path.name
         ext = file_path.suffix
 
-        if ext.lower() == ".heic":
-            # create an Image object from the HEIC file
-            print("Converting:", file_name)
-            try:
-                heif_file = pillow_heif.read_heif(file_path)
-            except ValueError:
-                print(f"File {file_name} could not be coverted.")
-                continue
-            image = Image.frombytes(
-                heif_file.mode,
-                heif_file.size,
-                heif_file.data,
-                "raw",)
+        if ext.lower() != ".heic":
+            continue
 
-            new_filename = file_path.stem + ".png"
-            new_filepath = images_dir / new_filename
-            image.save(new_filepath, format("png"))
+        # create an Image object from the HEIC file
+        print("Converting:", file_name)
+        try:
+            heif_file = pillow_heif.read_heif(file_path)
+        except ValueError:
+            print(f"File {file_name} could not be coverted.")
+            continue
+        image = Image.frombytes(
+            heif_file.mode,
+            heif_file.size,
+            heif_file.data,
+            "raw",)
 
-            try:
-                os.remove(file_path)
-            except:
-                print("Couldn't remove " + file_name)
+        new_filename = file_path.stem + ".png"
+        new_filepath = images_dir / new_filename
+        image.save(new_filepath, format("png"))
 
+        try:
+            os.remove(file_path)
+        except:
+            print("Couldn't remove " + file_name)
 
 
 def HEICtoJPG_no_del(images_dir: Path) -> None:
@@ -61,14 +63,16 @@ def HEICtoJPG_no_del(images_dir: Path) -> None:
         file_name = file_path.name
         ext = file_path.suffix
 
-        if ext.lower() == ".heic":
-            print("Converting:", file_name)
-            # TEMPPPPPP put this in try block!!!
-            img = Image.open(file_path)
+        if ext.lower() != ".heic":
+            continue
 
-            new_filename = file_path.stem + ".jpg"
-            new_filepath = images_dir / new_filename
-            img.save(new_filepath, format="JPEG")
+        print("Converting:", file_name)
+        # TEMPPPPPP put this in try block!!!
+        img = Image.open(file_path)
+
+        new_filename = file_path.stem + ".jpg"
+        new_filepath = images_dir / new_filename
+        img.save(new_filepath, format="JPEG")
 
 
 def HEICtoJPG_del(images_dir: Path) -> None:
@@ -76,19 +80,21 @@ def HEICtoJPG_del(images_dir: Path) -> None:
         file_name = file_path.name
         ext = file_path.suffix
 
-        if ext.lower() == ".heic":
-            print("Converting:", file_name)
-            # TEMPPPPPP put this in try block!!!
-            img = Image.open(file_path)
+        if ext.lower() != ".heic":
+            continue
 
-            new_filename = file_path.stem + ".jpg"
-            new_filepath = images_dir / new_filename
-            img.save(new_filepath, format="JPEG")
+        print("Converting:", file_name)
+        # TEMPPPPPP put this in try block!!!
+        img = Image.open(file_path)
 
-            try:
-                os.remove(file_path)
-            except:
-                print("Couldn't remove " + file_name)
+        new_filename = file_path.stem + ".jpg"
+        new_filepath = images_dir / new_filename
+        img.save(new_filepath, format="JPEG")
+
+        try:
+            os.remove(file_path)
+        except:
+            print("Couldn't remove " + file_name)
 
 
 def PNGtoJPG_no_del(images_dir: Path) -> None:
@@ -98,15 +104,17 @@ def PNGtoJPG_no_del(images_dir: Path) -> None:
         file_name = file_path.name
         ext = file_path.suffix
 
-        if ext.lower() == ".png":
-            print("Converting:", file_name)
-            # TEMPPPPPP put this in try block!!!
-            png_image = Image.open(file_path)
-            rgb_image = png_image.convert("RGB")
+        if ext.lower() != ".png":
+            continue
 
-            new_filename = file_path.stem + ".jpg"
-            new_filepath = images_dir / new_filename
-            rgb_image.save(new_filepath, "JPEG")
+        print("Converting:", file_name)
+        # TEMPPPPPP put this in try block!!!
+        png_image = Image.open(file_path)
+        rgb_image = png_image.convert("RGB")
+
+        new_filename = file_path.stem + ".jpg"
+        new_filepath = images_dir / new_filename
+        rgb_image.save(new_filepath, "JPEG")
 
 
 def PNGtoJPG_del(images_dir: Path) -> None:
@@ -116,17 +124,19 @@ def PNGtoJPG_del(images_dir: Path) -> None:
         file_name = file_path.name
         ext = file_path.suffix
 
-        if ext.lower() == ".png":
-            print("Converting:", file_name)
-            # TEMPPPPPP put this in try block!!!
-            png_image = Image.open(file_path)
-            rgb_image = png_image.convert("RGB")
+        if ext.lower() != ".png":
+            continue
 
-            new_filename = file_path.stem + ".jpg"
-            new_filepath = images_dir / new_filename
-            rgb_image.save(new_filepath, "JPEG")
+        print("Converting:", file_name)
+        # TEMPPPPPP put this in try block!!!
+        png_image = Image.open(file_path)
+        rgb_image = png_image.convert("RGB")
 
-            try:
-                os.remove(file_path)
-            except:
-                print("Couldn't remove " + file_name)
+        new_filename = file_path.stem + ".jpg"
+        new_filepath = images_dir / new_filename
+        rgb_image.save(new_filepath, "JPEG")
+
+        try:
+            os.remove(file_path)
+        except:
+            print("Couldn't remove " + file_name)
